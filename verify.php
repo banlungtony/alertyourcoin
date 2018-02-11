9819971d333f3b2da561e47fbc1e5194e052678d
Banlung Sirijampa <banlungtony@gmail.com>
2018-02-11 21:52:37 +0700

    add verity

---
+++ verify.php
@@ -0,0 +1,15 @@
+<?php
+$access_token = 't/0hmZR9lUaOSZu3nzMRtJA6xo97S+HmIfLBxihhtzkR1e0ZqEfyP2lJ4MDIGZlUZb5QJVlzv6UO+1ycWWjxH+G5Fb5SqwBmWGgdU8KkTuh780eEHa8FdExxG7GNlfE3pVAZ6qgVkHKC1jBsLRpSFwdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
